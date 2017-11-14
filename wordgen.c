5361ecfc778e98d73ac54df06ac1ac7fcdb5b4a3
Yingzhi <1995hyz@gmail.com>
2017-11-14 11:36:18 -0500

    Random Word Generator

---
+++ wordgen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main(int argc,char *argv[]){
+	int total=0;
+	int digits=0;
+	int endless=0;
+	int randomChar=0;
+	if((argc==1)||(strcmp(argv[1],"0")==0)){
+		endless=1;
+	}
+	else{
+		total=atoi(argv[1]);
+	}
+	while((total>0)||(endless)){
+		digits=(int)((((float) rand())/RAND_MAX)*7+3);//The word are 3 to 10 chars
+		int i=0;
+		while(i<digits){
+			randomChar=(int)((((float) rand())/RAND_MAX)*25+65);
+			fprintf(stdout,"%c",randomChar);
+			i++;
+		}
+		fprintf(stdout,"\n");
+		if(!endless){
+			total--;
+		}
+	}
+	//perror("");
+}
